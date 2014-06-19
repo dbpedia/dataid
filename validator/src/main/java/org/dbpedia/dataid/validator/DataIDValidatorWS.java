71a3aa4466c98a790aed4c1bed513bf495a51c17
Dimitris Kontokostas <jimkont@gmail.com>
2014-06-19 21:34:24 +0300

    revert

--- validator/src/main/java/org/dbpedia/dataid/validator/DataIDValidatorWS.java
+++ validator/src/main/java/org/dbpedia/dataid/validator/DataIDValidatorWS.java
@@ -24,6 +24,7 @@ import java.util.Arrays;
  */
 public class DataIDValidatorWS extends RDFUnitWebService {
 
+
     @Override
     public void init() throws ServletException {
         RDFUnitStaticWrapper.initWrapper("https://raw.githubusercontent.com/dbpedia/dataId/master/ontology/dataid.ttl");
@@ -32,30 +33,48 @@ public class DataIDValidatorWS extends RDFUnitWebService {
     @Override
     protected RDFUnitConfiguration getConfiguration(HttpServletRequest httpServletRequest) throws ParameterException {
 
-
         String type = httpServletRequest.getParameter("t");
         if (type == null || !(type.equals("text") || type.equals("uri"))) {
             throw new ParameterException("'t' must be one of text or uri");
         }
 
         String source = httpServletRequest.getParameter("s");
-        if (source == null || source.isEmpty()) {
+        if (source == null || source.isEmpty()){
             throw new ParameterException("'s' must be defined and not empty");
         }
 
+        boolean isText = type.equals("text");
+
         String datasetName = source;
-        if (type.equals("text")) {
+        if (isText) {
             datasetName = "custom-text";
-            throw new ParameterException("text not supported yet");
+        }
+
+        String inputFormat = "";
+        if (isText) {
+            inputFormat = httpServletRequest.getParameter("i");
+            if (inputFormat == null || inputFormat.isEmpty()) {
+                throw new ParameterException("'i' must be defined when -t = 'text'");
+            }
         }
 
         String outputFormat = httpServletRequest.getParameter("o");
-        if (outputFormat == null || outputFormat.isEmpty()) {
+        if (outputFormat == null || outputFormat.isEmpty()){
             outputFormat = "html";
         }
 
         RDFUnitConfiguration configuration = new RDFUnitConfiguration(datasetName, "../data/");
         configuration.setResultLevelReporting(TestCaseExecutionType.rlogTestCaseResult);
+
+        if (isText) {
+            try {
+                configuration.setCustomTextSource(source, inputFormat);
+            } catch (UndefinedSerializationException e) {
+                throw new ParameterException(inputFormat, e);
+            }
+
+        }
+
         try {
             configuration.setOutputFormatTypes(Arrays.asList(outputFormat));
         } catch (UndefinedSerializationException e) {
@@ -79,11 +98,12 @@ public class DataIDValidatorWS extends RDFUnitWebService {
     protected void printHelpMessage(HttpServletResponse httpServletResponse) throws IOException {
         String helpMessage =
                 "\n -t\ttype: One of 'text|uri'" +
-                        "\n -s\tsource: Depending on -t it can be either a uri or text" +
-                        "\n -i\tInput format (in case of text type):'turtle|ntriples|rdfxml" + //|JSON-LD|RDF/JSON|TriG|NQuads'" +
-                        "\n -o\tOutput format:'html(default)|turtle|jsonld|rdfjson|ntriples|rdfxml|rdfxml-abbrev" + //JSON-LD|RDF/JSON|TriG|NQuads'"
-                        "";
+                "\n -s\tsource: Depending on -t it can be either a uri or text" +
+                "\n -i\tInput format (in case of text type):'turtle|ntriples|rdfxml" + //|JSON-LD|RDF/JSON|TriG|NQuads'" +
+                "\n -o\tOutput format:'html(default)|turtle|jsonld|rdfjson|ntriples|rdfxml|rdfxml-abbrev" + //JSON-LD|RDF/JSON|TriG|NQuads'"
+                "";
 
     }
 }
+
 
